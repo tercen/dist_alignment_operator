74cd543b992a33ff91da131e59bcf673f6e47140
agouy <contact@agouy.com>
2020-10-13 20:06:20 +0000

    bug fix

--- main.R
+++ main.R
@@ -10,9 +10,11 @@ letters <- ctx$select(ctx$colors[[1]])[[1]]
 id_na_pos <- which(is.na(ctx$cselect(ctx$cnames[[1]]))) - 1
 if(length(id_na_pos) == 0) id_na_pos <- -1
 
-df <- ctx %>% select(.ri, .ci) %>% # filter(.ci != id_na_pos) %>% 
-  mutate(letter = letters) %>% filter(.ci != id_na_pos) %>% 
+df <- ctx %>% select(.ri, .ci) %>%
+  mutate(letter = letters) %>%
+  filter(.ci != id_na_pos) %>% 
   spread(.ci, letter)
+
 df[is.na(df)] <- "-"
 
 aln <- list()
@@ -23,29 +25,26 @@ aln$nb <- length(aln$nam)
 class(aln) <- "alignment"
 
 sequence_type <- "protein"
-if(!is.null(ctx$op.value('sequence_type'))) {
-  sequence_type <- ctx$op.value('sequence_type')
-} 
+if(!is.null(ctx$op.value('sequence_type'))) { sequence_type <- ctx$op.value('sequence_type') } 
 matrix <- "similarity"
-if(!is.null(ctx$op.value('matrix'))) {
-  matrix <- ctx$op.value('matrix')
-}
+if(!is.null(ctx$op.value('matrix'))) { matrix <- ctx$op.value('matrix') }
 gap <- 0
-if(!is.null(ctx$op.value('gap'))) {
-  gap <- as.numeric(ctx$op.value('gap'))
-}
+if(!is.null(ctx$op.value('gap'))) { gap <- as.numeric(ctx$op.value('gap')) }
+
 if(sequence_type != "protein" & matrix == "similarity") {
   stop("Similarity matrix only available for protein sequences.")
 }
-dist.aln <- dist.alignment(aln, matrix = matrix, gap = gap)
-xy <- t(combn(1:ncol(dist.aln), 2))
+
+dist.aln <- as.matrix(dist.alignment(aln, matrix = matrix, gap = gap))
+
 rnames <- ctx$rselect()[[1]]
-df_out <- data.frame(
-  .ri = xy[, 1] - 1,
-  dist_to = rnames[xy[, 2]],
-  dist = c(dist.aln)
-)
+
+df_out <- dist.aln %>%
+  as.data.frame %>%
+  mutate(.ri = 1:nrow(.) - 1) %>%
+  gather(.ci, dist, -.ri) %>% mutate(.ci = as.numeric(.ci))
+
 df_out %>%
-  rbind(data.frame(.ri = 1:length(rnames) - 1, dist_to = rnames, dist = 0)) %>%
+  mutate(dist_to = rnames[df_out$.ci + 1]) %>%
   ctx$addNamespace() %>%
   ctx$save()
